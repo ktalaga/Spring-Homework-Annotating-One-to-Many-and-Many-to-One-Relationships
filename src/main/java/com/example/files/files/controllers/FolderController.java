f0d699922fe69dc45f612c52ceb0d6c1d9cedfc0
konrad <ktalaga90@gmail.com>
2021-06-01 23:10:31 +0100

    folder get all get by id and post routes created

---
+++ src/main/java/com/example/files/files/controllers/FolderController.java
@@ -0,0 +1,34 @@
+package com.example.files.files.controllers;
+
+import com.example.files.files.models.Folder;
+import com.example.files.files.repositories.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class FolderController {
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @GetMapping(value = "folders")
+    public ResponseEntity<List<Folder>> getAllFolders(){
+        return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolderById(@PathVariable Long id){
+        return new ResponseEntity<>(folderRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/folders")
+    public ResponseEntity<Folder> postFolder(@RequestBody Folder folder){
+        folderRepository.save(folder);
+        return new ResponseEntity<>(folder, HttpStatus.CREATED);
+    }
+}
