b81e44e29ee5eaf48c3733961666612201cb7201
konrad <ktalaga90@gmail.com>
2021-06-01 22:53:47 +0100

    post route for user created, tested in insomnia, works fine

--- src/main/java/com/example/files/files/controllers/UserController.java
+++ src/main/java/com/example/files/files/controllers/UserController.java
@@ -5,9 +5,7 @@ import com.example.files.files.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +24,12 @@ public class UserController {
     @GetMapping(value = "/users/{id}")
     public ResponseEntity<Optional<User>> getUser(@PathVariable Long id){
         return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "users")
+    public ResponseEntity<User> postUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
 
